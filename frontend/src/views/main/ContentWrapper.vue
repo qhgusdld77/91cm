8ab74e15657e364fbbd618aca5b71ed3573b1eb1
lee <tyrfingr12@naver.com>
2020-06-10 13:09:52 +0900

    Invite 기능 ContentWrapper에서 분리

--- frontend/src/views/main/ContentWrapper.vue
+++ frontend/src/views/main/ContentWrapper.vue
@@ -15,7 +15,6 @@
         </div>
       </a>
       <div class="c-i-wrapper">
-        <!-- 더 뭔가 추가할 거 같아서 div로 감싸놓음 -->
         <div style="flex-grow:1;" class="myflex-column">
           <div style="position: relative;">
             <div class="mytextarea-wrapper" v-if="!$store.state.isInviteMode && !$store.state.isSearchMode">
@@ -39,63 +38,21 @@
                 @keydown.shift.alt.50='inviteToggle'
               ></b-form-textarea>
             </div>
-
+            <!--  초대 모드 시작 -->
             <div v-if="$store.state.isInviteMode">
-<!--              <InviteInput @send="send"></InviteInput>-->
-              <v-row>
-                <v-col cols="12">
-                  <v-autocomplete
-                    v-model="friends"
-                    :items="userList"
-                    @keydown.enter.exact="enter"
-                    @keydown.esc.exact="inviteToggle"
-                    filled
-                    autofocus
-                    chips
-                    label="Select"
-                    item-text="name"
-                    item-value="email"
-                    multiple
-                    :menu-props="{  contentClass: 'inviteClass'}"
-                  >
-                    <template v-slot:selection="data">
-                      <v-chip
-                        v-bind="data.attrs"
-                        :input-value="data.selected"
-                        close
-                        @click="data.select"
-                        @click:close="remove(data.item)"
-                      >
-                        <v-avatar left>
-                          <v-img :src="data.item.picture"></v-img>
-                        </v-avatar>
-                        {{ data.item.name }}
-                      </v-chip>
-                    </template>
-                    <template v-slot:item="data">
-                      <template v-if="typeof data.item !== 'object'">
-                        <v-list-item-content v-text="data.item"></v-list-item-content>
-                      </template>
-                      <template v-else>
-                        <v-list-item-avatar>
-                          <img :src="data.item.picture">
-                        </v-list-item-avatar>
-                        <v-list-item-content>
-                          <v-list-item-title v-html="data.item.name"></v-list-item-title>
-                          <v-list-item-subtitle v-html="data.item.group"></v-list-item-subtitle>
-                        </v-list-item-content>
-                      </template>
-                    </template>
-                  </v-autocomplete>
-                </v-col>
-              </v-row>
+              <InviteInput @send="send" @inviteToggle="inviteToggle" :message="message"></InviteInput>
             </div>
+            <!-- 초대 모드 끝  -->
+            <!-- 채팅 검색 모드 시작 -->
             <SearchInput
               :cursorPoint="cursorPoint"
               :wrapperEl="wrapperEl"
-              @getMessage="getMessage"></SearchInput>
+              @getMessage="getMessage">
+            </SearchInput>
+            <!-- 채팅 검색 모드 끝 -->
           </div>
           <div style="display: flex;flex-grow: 1;">
+            <!-- 파일 업로드 progress bar -->
             <v-progress-linear
               v-if="isFileUpload"
               color="cyan darken-4"
@@ -118,10 +75,8 @@
 </template>
 <script>
   import MsgBox from './MsgBox'
-  import InviteService from '../../service/inviteService'
   import CommonClass from '../../service/common'
   import SearchInput from './SearchInput'
-  import AboutChannel from '../../service/aboutchannel'
   import {mapGetters} from "vuex";
   import InviteInput from "../../components/InviteInput";
 
@@ -135,7 +90,6 @@
       return {
         isFileUpload: false,
         progressValue: 0,
-        friends: [],
         sendMail: false,
         tempImg: '',
         stringByteLength: 0,
@@ -186,61 +140,23 @@
       if (this.$store.state.oldComponent != 'main' && this.$store.state.selectComponent == 'main') {
         this.scrollToEnd(true)
       }
-      this.friends = []
       this.$store.state.isInviteMode = false
       this.$store.state.isSearchMode = false
     },
     methods: {
-      enter: async function (event) {
+      inviteToggle: function (e) {
         let el = document.querySelector(".menuable__content__active.inviteClass")
-        if (el == null) {
-          if (this.friends.length != 0) {
-            await InviteService.invite(this.$store.state.currentUser.email, this.$store.state.currentChannel.id, this.friends)
-              .then(res => {
-                const invite = {
-                  channel_id: this.$store.state.currentChannel.id,
-                  sender: this.$store.state.currentUser.email,
-                  recipients: this.friends
-                }
-                for (let i = 0; i < this.friends.length; i++) {
-                  const user = this.userList.find(el => el.email == this.friends[i])
-                  this.message.content += user.name + '님 '
-                }
-
-                //메일 오류 계속 떠서 일단 임시로 주석 처리함
-                this.$http.post('/api/invite/mail', invite)
-                  .then(res => {
-                    console.log(res.data)
-                  })
-                this.message.content += '을 초대했습니다.'
-                this.$eventBus.$emit('getUserList', true)
-                this.send()
-                this.inviteDataInit()
-
-              }).catch(error => {
-                let alertmsg = ''
-                if (error.response.data.list != null) {
-                  const alertList = error.response.data.list
-                  for (let i = 0; i < alertList.length; i++) {
-                    const user = this.userList.find(el => el.email == alertList[i])
-                    alertmsg += user.name + '님 '
-                  }
-                  alertmsg += '은 이미 이 채널에 초대 받았습니다. 확인해주세요.'
-                  this.$alertModal('error', alertmsg)
-                } else {
-                  this.$alertModal('error', error.response.data.message)
-                }
-                console.error(error.response)
-                this.message.content = ''
-              })
-          } else {
-            this.$alertModal('alert', '초대할 사용자를 선택해주세요')
+        if (this.$store.state.isInviteMode == false) {
+          this.$store.state.isInviteMode = !this.$store.state.isInviteMode
+        } else {
+          if (el == null) {
+            this.inviteDataInit()
           }
         }
       },
-      remove(item) {
-        const index = this.friends.indexOf(item.email);
-        if (index >= 0) this.friends.splice(index, 1);
+      inviteDataInit: function () {
+        this.message.content = ''
+        this.$store.state.isInviteMode = !this.$store.state.isInviteMode
       },
       sendMailToggle() {
         this.sendMail = !this.sendMail
@@ -303,21 +219,6 @@
           this.$alertModal('error', '폴더는 업로드 할 수 없습니다.')
         })
       },
-      inviteToggle: function (e) {
-        let el = document.querySelector(".menuable__content__active.inviteClass")
-        if (this.$store.state.isInviteMode == false) {
-          this.$store.state.isInviteMode = !this.$store.state.isInviteMode
-        } else {
-          if (el == null) {
-            this.inviteDataInit()
-          }
-        }
-      },
-      inviteDataInit: function () {
-        this.friends = []
-        this.message.content = ''
-        this.$store.state.isInviteMode = !this.$store.state.isInviteMode
-      },
       send: async function (e) {
         if (e != null) {
           e.preventDefault()
@@ -332,7 +233,7 @@
             this.message.content = ''
             this.scrollToEnd(true)
             if (this.sendMail) {
-              console.log(this.$store.state.channelUsers.filter(channelUser => channelUser != this.$store.state.currentUser))
+              // console.log(this.$store.state.channelUsers.filter(channelUser => channelUser != this.$store.state.currentUser))
               this.$store.state.channelUsers.filter(channelUser => channelUser != this.$store.state.currentUser)
                 .forEach(channelUser => {
                   this.$http.post('/api/message/send/mail', {
@@ -349,14 +250,12 @@
             this.message.content = CommonClass.replaceErrorMsg(this.message.content)
             this.message.content = '<p style="color:red;">메세지 전송에 실패하였습니다.</p>' + this.message.content
             let errormsg = JSON.parse(JSON.stringify(this.message))
-            //dd
             this.$store.commit('pushMsg', errormsg)
             this.message.content = ''
           }
         }
       },
       scrollEvt(e) {
-
         let element = e.target;
         //스크롤이 없을때에도 스크롤 위치가 최상단이기 때문에 스크롤이 있는지 없는지 판단해야한다.
         if (element.scrollTop <= 0 && element.scrollHeight != element.clientHeight) {
@@ -367,7 +266,6 @@
           this.msgPreviewBool = false
         }
       },
-
       getMessage: function (wrapperEl) {
         this.cursorPoint.channel_id = this.$store.state.currentChannel.id
         this.$http.post('/api/message/getmsg', JSON.stringify(this.cursorPoint), {
@@ -421,7 +319,6 @@
         this.msgPreviewBool = false
       },
       initData() {
-        this.friends = []
         this.$store.state.isInviteMode = false
         this.$store.state.isSearchMode = false
         this.message.channel_id = this.getCurrentChannel.id
