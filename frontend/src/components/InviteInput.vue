468636d43834c2d9f72d2bf61d632c50a5158888
jny9708 <jny9708@gmail.com>
2020-07-08 14:04:07 +0900

    enter(초대함수), channel details 삭제

--- frontend/src/components/InviteInput.vue
+++ frontend/src/components/InviteInput.vue
@@ -75,59 +75,7 @@
       remove(item) {
         const index = this.friends.indexOf(item.email);
         if (index >= 0) this.friends.splice(index, 1);
-      },
-      enter: async function (event) {
-        return
-        let el = document.querySelector(".menuable__content__active.inviteClass")
-        if (el == null) {
-          if (this.friends.length != 0) {
-            await InviteService.invite(this.$store.state.currentUser.email, this.$store.state.currentChannel.id, this.friends)
-              .then(res => {
-                for (let i = 0; i < this.friends.length; i++) {
-                  const user = this.inviteUserList.find(el => el.email == this.friends[i])
-
-                  if (user != null) {
-                    this.message.content += user.name + '님'
-                  }
-                }
-                // 임시 주석처리
-                // this.$http.post('/api/invite/mail', {
-                //   channel_id: this.$store.state.currentChannel.id,
-                //   sender: this.$store.state.currentUser.email,
-                //   recipients: this.friends
-                // })
-                //   .then(res => {
-                //     console.warn(res.data)
-                //   })
-                this.message.content += '을 초대했습니다.'
-                //?
-                this.$eventBus.$emit('getUserList', true)
-                this.$emit('sendMessage', null, true)
-                this.friends = []
-                this.message.content = ''
-                this.$store.state.isInviteMode = !this.$store.state.isInviteMode
-              }).catch(error => {
-                let alertmsg = ''
-                console.log(error, 'error')
-                if (error.response.data.list != null) {
-                  const alertList = error.response.data.list
-                  for (let i = 0; i < alertList.length; i++) {
-                    const user = this.userList.find(el => el.email == alertList[i])
-                    alertmsg += user.name + '님'
-                  }
-                  alertmsg += '은 이미 이 채널에 초대 받았습니다. 확인해주세요.'
-                  this.$_error(alertmsg)
-                } else {
-                  this.$_error(error.response.data.message)
-                }
-                console.error(error.response)
-                this.message.content = ''
-              })
-          } else {
-            this.$_alert('초대할 사용자를 선택해주세요')
-          }
-        }
-      },
+      }
     }
   }
 </script>
