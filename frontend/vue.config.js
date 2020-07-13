8a710fe231462d5a4c777101634aa1271899e5bc
lee <tyrfingr12@naver.com>
2020-07-13 17:01:36 +0900

    vue config 파일 변경

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,45 +2,45 @@ const path = require('path')
 
 module.exports = {
   lintOnSave: false,
-  // productionSourceMap: false,
+  productionSourceMap: false,
   outputDir: path.resolve(__dirname, '../' + 'src/main/resources/static'),
   devServer: {
-    https: true,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:9191',
-        // ws: false,
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      },
-      '/oauth2':{
-        target: 'https://localhost:9191',
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      },
-      '/endpoint':{
-        target: 'https://localhost:9191',
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      },
-      '/login':{
-        target: 'https://localhost:9191',
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      }
-    }
+    // https: true,
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://localhost:9191',
+    //     // ws: false,
+    //     changeOrigin: true,
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    //     }
+    //   },
+    //   '/oauth2':{
+    //     target: 'https://localhost:9191',
+    //     changeOrigin: true,
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    //     }
+    //   },
+    //   '/endpoint':{
+    //     target: 'https://localhost:9191',
+    //     changeOrigin: true,
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    //     }
+    //   },
+    //   '/login':{
+    //     target: 'https://localhost:9191',
+    //     changeOrigin: true,
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    //     }
+    //   }
+    // }
   },
   configureWebpack: {
     entry: ['babel-polyfill', './src/main.js']
